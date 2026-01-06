2ad765e7c9416ed6841fb5d171b04476a895b3b7
STVcybergameunity <cybergameunity@gmail.com>
2026-01-06 13:04:11 +0100

    Add files via upload

---
+++ src/Player.cs
@@ -0,0 +1,13 @@
+class Player
+{
+    //fields
+    private int health;
+    // auto property
+    public Room CurrentRoom { get; set; }
+    // constructor
+    public Player()
+    {
+        CurrentRoom = null;
+        health = 100;
+    }
+}
