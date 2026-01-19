78bcdd44a8160a59d7e44b0de7b02b80408aea81
STVcybergameunity <cybergameunity@gmail.com>
2026-01-19 09:13:03 +0100

    Add Inventory and health methods to Player class
    Refactor Player class to include Inventory and health management methods.

--- src/Player.cs
+++ src/Player.cs
@@ -1,15 +1,54 @@
+using System.Runtime.InteropServices;
+
 class Player
 {
-    private Game game;
     //fields
+    private Inventory backpack;
     public int health;
     // auto property
     public Room CurrentRoom { get; set; }
     // constructor
+
+    // Makes a player with HP and a Inventory
     public Player()
     {
         CurrentRoom = null;
         health = 100;
+        backpack = new Inventory(100);
     }
-    //methods
+    // Methods
+
+    // Allows u to take a item from a room
+    public bool TakeFromChest(string itemName)
+    {
+        return false;
+    }
+
+    // A method that allows you to take damage
+    // Can later be inplemented for on a hit taken /Maybe with random int
+    public void Damage()
+	{
+		health -=10;
+	}
+
+    // Heals the player but only if u can heal 20 hp or more
+    // Else print feedback
+    public void Heal()
+	{
+		if (health <= 80)
+		{
+			health += 20;
+			Console.WriteLine($"You healed! Your health is now: {health}HP");
+		}
+		else
+		{
+			Console.WriteLine("You aren't all that injured are you?");
+		}
+	}
+
+    // Kill urself usefull to test on death triggers
+    public void Sepuccu()
+	{
+		health = 0;
+	}
 }
