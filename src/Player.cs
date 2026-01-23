27df1c1d89a050fed7e9138a523da48bc44e1204
STVcybergameunity <cybergameunity@gmail.com>
2026-01-23 22:36:28 +0100

    Update Player class methods and properties
    Refactor Player class methods for item management and healing

--- src/Player.cs
+++ src/Player.cs
@@ -5,7 +5,7 @@ class Player
     //fields
     private Inventory backpack;
     public int health;
-    // auto property
+        // auto property
     public Room CurrentRoom { get; set; }
     // constructor
 
@@ -17,29 +17,28 @@ class Player
         backpack = new Inventory(100);
     }
     // Methods
-
     public Inventory getBackpack()
     {
         return backpack;
     }
 
-    // Allows u to take a item from a room
-    public bool TakeFromChest(string itemName)
+    public Item Place(string itemName)
     {
-        if (CurrentRoom.Chest != null)
+        // Een tijdelijke item object
+        Item temp = null;
+
+        if(backpack.getItems().ContainsKey(itemName))
         {
-            // Remove itemName from chest and save it
-            Item item = CurrentRoom.Chest.Get(itemName);
+            // Item bestaat in dictionary
+            // Eerst veilig stellen in temp
+            temp = backpack.getItems()[itemName];
 
-            // Add the item we took to the backpack
-            backpack.Put(itemName, item);
-
-            // Return true
-            return true;
+            // Uit dictionary van huidige Inventory halen
+            backpack.getItems().Remove(itemName);
         }
 
-        // If empty return false
-        return false;
+        // Veiliggestelde item teruggeven.
+        return temp;
     }
 
     // A method that allows you to take damage
@@ -49,24 +48,75 @@ class Player
 		health -=10;
 	}
 
-    // Heals the player but only if u can heal 20 hp or more
+    // Heals the player but only if u can heal with a amount of hp u choose
     // Else print feedback
-    public void Heal(int hptot)
+    public void Heal(string hptotstr)
 	{
-		if (health <= 100-hptot)
+        if (hptotstr != null)
+        {
+            int hptot=Int32.Parse(hptotstr);
+            if (health <= 100-hptot)
+            {
+                health += hptot;
+                Console.WriteLine($"You healed! Your health is now: {health}HP");
+            }
+            else
+            {
+                Console.WriteLine("You aren't all that injured are you?");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please add a valid number...");
+        }
+	}
+
+    // If the player is low show a message
+	// If you are very low show a diffrent message
+	public void LowHp()
+	{
+		if (health <= 40 && health >= 30)
 		{
-			health += hptot;
-			Console.WriteLine($"You healed! Your health is now: {health}HP");
+			Console.WriteLine($"U feel hurt.");
 		}
-		else
+		else if(health <= 20)
 		{
-			Console.WriteLine("You aren't all that injured are you?");
+			Console.WriteLine($"U feel miserable. U should heal!");
 		}
 	}
+
+    // Shows the players HP
+	public void SeeHealth()
+    {
+        Console.WriteLine($"Your health is: {health}HP");
+    }
 
     // Kill urself usefull to test on death triggers
     public void Sepuccu()
 	{
 		health = 0;
 	}
+
+
+    // Uses a item
+    public void use(Command command)
+    {
+        if(!command.HasSecondWord())
+        {
+            Console.WriteLine("What do u want to use.");
+        }
+
+        string itemName = command.SecondWord;
+        Item item = backpack.peek(itemName);
+        if (item == null)
+        {
+            Console.WriteLine("U don't have that as a item.");
+        }
+        backpack.Get(itemName);
+        
+        if (itemName == "bandage")
+        {
+            Console.WriteLine();
+        }
+    }
 }
